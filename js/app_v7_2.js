cd7b3c2c401e61a2e44c02cd72808e6d7ea3716e
limerickgaahub <limerickgaahub@gmail.com>
2025-08-23 21:27:10 +0100

    Delete js/app_v7_2.js

--- js/app_v7_2.js
+++
@@ -1,6 +0,0 @@
-console.log('App v7.2 loaded');
-// This is a placeholder JS - full dropdown, table, and fixtures logic to be implemented.
-fetch('data/hurling_2025.json')
-  .then(r => r.json())
-  .then(data => console.log('Loaded fixtures:', data.length))
-  .catch(err => console.error('Error loading JSON', err));
