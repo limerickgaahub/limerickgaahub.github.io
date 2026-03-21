6b95fadd4e02463f0acb3e0b05799cdcabd1d036
limerickgaahub <limerickgaahub@gmail.com>
2026-03-21 13:17:33 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,12 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request));
+});
