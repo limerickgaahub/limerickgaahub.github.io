9f1cf5518adb2622c3826a532c5a77897a8c1440
limerickgaahub <limerickgaahub@gmail.com>
2026-03-28 23:55:49 +0000

    Update scrape_league_fixtures.py

--- scripts/scrape_league_fixtures.py
+++ scripts/scrape_league_fixtures.py
@@ -570,12 +570,13 @@ def parse_league_results(lines: List[str]) -> List[LeagueFixture]:
             if WO_RE.match(lines[j].strip()):
                 if (
                     home is not None and
-                    away is not None and
                     d is not None and
-                    home_goals is None and
-                    home_points is None and
-                    away_goals is None and
-                    away_points is None
+                    not (
+                        home_goals is not None and
+                        home_points is not None and
+                        away_goals is not None and
+                        away_points is not None
+                    )
                 ):
                     status = "Walkover"
                 j += 1
